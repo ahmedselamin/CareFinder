0591cd71ebe5dae2550d8ab0ea77e787158cfafd
eddyiiz <eddyxsayf@gmail.com>
2024-11-17 22:13:31 +0200

    add

---
+++ CareFinder.Server/DTOs/RegisterDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace CareFinder.Server.DTOs
+{
+    public class RegisterDTO
+    {
+        public required string Email { get; set; }
+        public required string FullName { get; set; }
+        public required string Password { get; set; }
+        public required string Specialty { get; set; }
+        public required string PracticeName { get; set; }
+        public required string City { get; set; }
+        public required List<string> Qualifications { get; set; } = new List<string>();
+        public required string About { get; set; }
+    }
+}
