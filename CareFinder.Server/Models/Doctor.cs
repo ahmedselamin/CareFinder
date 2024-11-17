62e23604877d13a99c0b79747c4da3eef3f26b88
eddyiiz <eddyxsayf@gmail.com>
2024-11-17 10:43:54 +0200

    add doctor model

---
+++ CareFinder.Server/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿namespace CareFinder.Server.Models
+{
+    public class Doctor : User
+    {
+        [Required]
+        public string Specialty { get; set; } = string.Empty;
+        [Required]
+        public string PracticeName { get; set; } = string.Empty;
+        [Required]
+        public string City { get; set; } = string.Empty;
+        [Required]
+        public string Address { get; set; } = string.Empty;
+        [Required]
+        public required List<string> Qualifications { get; set; } = new List<string>();
+        [Required]
+        public string About { get; set; } = string.Empty;
+    }
+}
