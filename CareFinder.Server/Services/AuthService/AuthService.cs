1b9490376391ee5b81d9946b0d2d1c816a5ac1d9
eddyiiz <eddyxsayf@gmail.com>
2024-11-17 22:02:50 +0200

    add Register

--- CareFinder.Server/Services/AuthService/AuthService.cs
+++ CareFinder.Server/Services/AuthService/AuthService.cs
@@ -1,6 +1,111 @@
-﻿namespace CareFinder.Server.Services.AuthService
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace CareFinder.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
+        private readonly DataContext _context;
+
+        public IConfiguration _configuration { get; }
+
+        public AuthService(DataContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        public async Task<ServiceResponse<int>> Register(Doctor doctor, string password)
+        {
+            var response = new ServiceResponse<int>();
+
+            try
+            {
+                CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+                doctor.PasswordHash = passwordHash;
+                doctor.PasswordSalt = passwordSalt;
+
+                await _context.Doctors.AddAsync(doctor);
+                await _context.SaveChangesAsync();
+
+                response.Data = doctor.Id;
+                response.Message = "Please log in";
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+
+                return response;
+            }
+        }
+
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>();
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+
+                return response;
+            }
+        }
+
+        private void CreatePasswordHash(string passowrd, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passowrd));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computeHash =
+                    hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                return computeHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private string CreateToken(Doctor doctor)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, doctor.Id.ToString()),
+                new Claim(ClaimTypes.Email, doctor.Email)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                    .GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken
+                (
+                  claims: claims,
+                  //expires: DateTime.Now.AddHours(1),
+                  expires: DateTime.Now.AddDays(1),
+                  signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
     }
 }
