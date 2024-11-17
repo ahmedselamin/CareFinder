5b5da692c0cdb848cf957eef45d11b1b6162be3c
eddyiiz <eddyxsayf@gmail.com>
2024-11-17 10:59:40 +0200

    add admin

---
+++ CareFinder.Server/Models/Admin.cs
@@ -0,0 +1,7 @@
+﻿namespace CareFinder.Server.Models
+{
+    public class Admin : User
+    {
+        public string Role { get; set; } = "Admin";
+    }
+}
--- CareFinder.Server/Models/Doctor.cs
+++ CareFinder.Server/Models/Doctor.cs
@@ -2,17 +2,11 @@
 {
     public class Doctor : User
     {
-        [Required]
         public string Specialty { get; set; } = string.Empty;
-        [Required]
         public string PracticeName { get; set; } = string.Empty;
-        [Required]
         public string City { get; set; } = string.Empty;
-        [Required]
         public string Address { get; set; } = string.Empty;
-        [Required]
-        public required List<string> Qualifications { get; set; } = new List<string>();
-        [Required]
+        public List<string> Qualifications { get; set; } = new List<string>();
         public string About { get; set; } = string.Empty;
     }
 }
