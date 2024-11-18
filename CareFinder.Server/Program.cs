433c5c6990c1bd42e8c4158e54834d7fbcd70994
eddyiiz <eddyxsayf@gmail.com>
2024-11-18 17:43:02 +0200

    add memory cache

--- CareFinder.Server/Program.cs
+++ CareFinder.Server/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddMemoryCache();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IDoctorService, DoctorService>();
 
