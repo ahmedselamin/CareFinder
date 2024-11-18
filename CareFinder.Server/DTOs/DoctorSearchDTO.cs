438ecda3de5bbfe9ddee2d6b17a15fe7d694cbec
eddyiiz <eddyxsayf@gmail.com>
2024-11-18 17:02:17 +0200

    add search dto

---
+++ CareFinder.Server/DTOs/DoctorSearchDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CareFinder.Server.DTOs
+{
+    public class DoctorSearchDTO
+    {
+        public int Id { get; set; }
+        public required string FullName { get; set; }
+        public required string Specialty { get; set; }
+        public required string City { get; set; }
+    }
+}
