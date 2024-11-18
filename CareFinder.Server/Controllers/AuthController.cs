2fa36ced0cba4035fe510a754d55cc61ee923910
eddyiiz <eddyxsayf@gmail.com>
2024-11-18 10:08:58 +0200

    add auth api

---
+++ CareFinder.Server/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CareFinder.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+    }
+}
