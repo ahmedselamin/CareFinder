ded0c6fd06b57cf61a29efc0b6faadce5f3fd9e5
eddyiiz <eddyxsayf@gmail.com>
2024-11-18 13:53:15 +0200

    add get doctor

---
+++ CareFinder.Server/Controllers/DoctorsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CareFinder.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorsController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+
+        [HttpGet("/{doctorId:int}")]
+        public async Task<ActionResult> GetDoctor(int doctorId)
+        {
+            var response = await _doctorService.GetDoctor(doctorId);
+
+            return response.Success ? Ok(response) : BadRequest(response.Message);
+        }
+    }
+}
