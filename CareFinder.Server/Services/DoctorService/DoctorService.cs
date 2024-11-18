45da1e064348c3660391dbfbdf39df226f9b37fa
eddyiiz <eddyxsayf@gmail.com>
2024-11-18 13:42:56 +0200

    add get doctor

--- CareFinder.Server/Services/DoctorService/DoctorService.cs
+++ CareFinder.Server/Services/DoctorService/DoctorService.cs
@@ -1,6 +1,46 @@
-﻿namespace CareFinder.Server.Services.DoctorService
+﻿
+namespace CareFinder.Server.Services.DoctorService
 {
     public class DoctorService : IDoctorService
     {
+        private readonly DataContext _context;
+
+        public DoctorService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ServiceResponse<Doctor>> GetDoctor(int doctorId)
+        {
+            var response = new ServiceResponse<Doctor>();
+
+            try
+            {
+                var doctor = await _context.Doctors.FirstOrDefaultAsync(d => d.Id == doctorId);
+
+                if (doctor == null)
+                {
+                    response.Success = false;
+                    response.Message = "Not found";
+
+                    return response;
+                }
+
+                response.Data = doctor;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+
+                return response;
+            }
+        }
+
+        public Task<ServiceResponse<List<Doctor>>> SearchDoctors(string searchText)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
