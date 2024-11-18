de2902e532e25b26b8ca411aa7c0cd08b9c6e69e
eddyiiz <eddyxsayf@gmail.com>
2024-11-18 10:24:39 +0200

    add login dto

---
+++ CareFinder.Server/DTOs/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CareFinder.Server.DTOs
+{
+    public class LoginDTO
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
