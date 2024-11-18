25d5873f2fe5b94dc116c078809b5d78dd220460
eddyiiz <eddyxsayf@gmail.com>
2024-11-18 10:09:11 +0200

    add register

--- CareFinder.Server/Controllers/AuthController.cs
+++ CareFinder.Server/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CareFinder.Server.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CareFinder.Server.Controllers
 {
@@ -6,6 +7,32 @@ namespace CareFinder.Server.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
 
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] RegisterDTO request)
+        {
+            var response = await _authService
+                .Register(
+                    new Doctor
+                    {
+                        Email = request.Email,
+                        FullName = request.FullName,
+                        Specialty = request.Specialty,
+                        PracticeName = request.PracticeName,
+                        City = request.City,
+                        Qualifications = request.Qualifications,
+                        About = request.About,
+                    },
+                    request.Password
+                );
+
+            return response.Success ? Ok(response) : BadRequest();
+        }
     }
 }
