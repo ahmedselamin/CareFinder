fc58f24ddf7c2ffe2e088836273ec17f08494b1a
eddyiiz <eddyxsayf@gmail.com>
2024-11-19 10:00:48 +0200

    add availability slot

--- CareFinder.Server/Data/DataContext.cs
+++ CareFinder.Server/Data/DataContext.cs
@@ -3,5 +3,14 @@
     public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
     {
         public required DbSet<Doctor> Doctors { get; set; }
+        public required DbSet<AvailabilitySlot> AvailabilitySlots { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>()
+                .HasMany(d => d.AvailabilitySlots)
+                .WithOne()
+                .HasForeignKey(a => a.DoctorId);
+        }
     }
 }
