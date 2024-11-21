871c9ec3ab0186363daa1745ff418435d76cbb64
eddyiiz <eddyxsayf@gmail.com>
2024-11-21 20:19:47 +0200

    Add Appointments

--- CareFinder.Server/Migrations/20241121181813_AppointmentsCreate.Designer.cs
+++ CareFinder.Server/Migrations/20241121181813_AppointmentsCreate.Designer.cs
@@ -4,6 +4,7 @@ using CareFinder.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CareFinder.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241121181813_AppointmentsCreate")]
+    partial class AppointmentsCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CareFinder.Server/Migrations/20241121181813_AppointmentsCreate.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareFinder.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppointmentsCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_DoctorId",
+                table: "AvailabilitySlots");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentId",
+                table: "AvailabilitySlots",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    TimeSlotId = table.Column<int>(type: "int", nullable: false),
+                    PatientFullName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointments_AvailabilitySlots_TimeSlotId",
+                        column: x => x.TimeSlotId,
+                        principalTable: "AvailabilitySlots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_TimeSlotId",
+                table: "Appointments",
+                column: "TimeSlotId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_DoctorId",
+                table: "AvailabilitySlots",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_DoctorId",
+                table: "AvailabilitySlots");
+
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentId",
+                table: "AvailabilitySlots");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AvailabilitySlots_Doctors_DoctorId",
+                table: "AvailabilitySlots",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
