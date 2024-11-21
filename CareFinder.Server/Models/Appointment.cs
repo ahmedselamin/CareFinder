bdfcc7fc2ef691aaf77eb07ef622eff2c1b45d5c
eddyiiz <eddyxsayf@gmail.com>
2024-11-21 16:17:59 +0200

    add model

---
+++ CareFinder.Server/Models/Appointment.cs
@@ -0,0 +1,12 @@
+﻿namespace CareFinder.Server.Models
+{
+    public class Appointment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int DoctorId { get; set; }
+        public int TimeSlotId { get; set; }
+        public required string PatientFullName { get; set; }
+    }
+}
