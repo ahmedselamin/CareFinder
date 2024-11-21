295efe39014daf5f51c5075c67dca8d4b494568c
eddyiiz <eddyxsayf@gmail.com>
2024-11-21 17:14:41 +0200

    add dto

---
+++ CareFinder.Server/DTOs/CreateAppointmentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CareFinder.Server.DTOs
+{
+    public class CreateAppointmentDTO
+    {
+        public int DoctorId { get; set; }
+        public int TimeSlotId { get; set; }
+        public required string PatientsFullName { get; set; }
+    }
+}
