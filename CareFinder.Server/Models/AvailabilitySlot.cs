42c30fabe007594dce6495e2463a129ab79b34ac
eddyiiz <eddyxsayf@gmail.com>
2024-11-21 16:23:16 +0200

    update with appointment Id

--- CareFinder.Server/Models/AvailabilitySlot.cs
+++ CareFinder.Server/Models/AvailabilitySlot.cs
@@ -6,6 +6,7 @@
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int DoctorId { get; set; }
+        public int AppointmentId { get; set; }
         public required string Day { get; set; }
         public TimeOnly StartsAt { get; set; }
         public TimeOnly EndsAt { get; set; }
