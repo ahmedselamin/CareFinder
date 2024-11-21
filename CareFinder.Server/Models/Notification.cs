fc5da8d08e1bc2d89f83a676c4933a0af08ebdcd
eddyiiz <eddyxsayf@gmail.com>
2024-11-21 21:47:25 +0200

    add model

---
+++ CareFinder.Server/Models/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace CareFinder.Server.Models
+{
+    public class Notification
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int DoctorId { get; set; }
+        public required string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
