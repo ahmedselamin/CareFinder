8f4df473c6cecd826a681560297ffbd82fc19900
eddyiiz <eddyxsayf@gmail.com>
2024-11-21 21:55:42 +0200

    add fetch all notifications

--- CareFinder.Server/Services/NotificationService/NotificationService.cs
+++ CareFinder.Server/Services/NotificationService/NotificationService.cs
@@ -1,6 +1,41 @@
-﻿namespace CareFinder.Server.Services.NotificationService
+﻿
+namespace CareFinder.Server.Services.NotificationService
 {
     public class NotificationService : INotificationService
     {
+        private readonly DataContext _context;
+
+        public NotificationService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<ServiceResponse<List<Notification>>> FetchAllNotification(int doctorId)
+        {
+            var response = new ServiceResponse<List<Notification>>();
+
+            try
+            {
+                var notifications = await _context.Notifications
+                    .Where(n => n.DoctorId == doctorId)
+                    .OrderByDescending(n => n.Timestamp)
+                    .ToListAsync();
+
+                response.Data = notifications;
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+
+                return response;
+            }
+        }
+
+        public Task<ServiceResponse<bool>> SendNofication(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
