6db86cf76a38027c5c9a4d78ced47369b0d98b31
eddyiiz <eddyxsayf@gmail.com>
2024-11-21 21:43:50 +0200

    add notification service

---
+++ CareFinder.Server/Services/NotificationService/INotificationService.cs
@@ -0,0 +1,6 @@
+﻿namespace CareFinder.Server.Services.NotificationService
+{
+    public interface INotificationService
+    {
+    }
+}
---
+++ CareFinder.Server/Services/NotificationService/NotificationService.cs
@@ -0,0 +1,6 @@
+﻿namespace CareFinder.Server.Services.NotificationService
+{
+    public class NotificationService : INotificationService
+    {
+    }
+}
