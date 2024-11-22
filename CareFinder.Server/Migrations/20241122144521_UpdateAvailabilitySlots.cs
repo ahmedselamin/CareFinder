40f4cc1c9645cbfb9e2a2eab5b50961d80e5cd1c
eddyiiz <eddyxsayf@gmail.com>
2024-11-22 16:47:20 +0200

    update availability slots

---
+++ CareFinder.Server/Migrations/20241122144521_UpdateAvailabilitySlots.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareFinder.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAvailabilitySlots : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BreakInterval",
+                table: "AvailabilitySlots",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BreakInterval",
+                table: "AvailabilitySlots");
+        }
+    }
+}
